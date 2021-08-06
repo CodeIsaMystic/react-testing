2eeb81defad741504088b971406c020942b38a1e
Julien Orjollet <oj.professionnel@gmail.com>
2021-08-06 13:54:32 +0200

    adjusting styles to purple

--- apps/color-button/src/App.js
+++ apps/color-button/src/App.js
@@ -3,17 +3,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
-  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+  const [buttonColor, setButtonColor] = useState("rebeccapurple");
+  const newButtonColor =
+    buttonColor === "rebeccapurple" ? "whitesmoke" : "rebeccapurple";
 
   return (
-    <div>
+    <div className="app-container">
       <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
       >
         Change to {newButtonColor}
       </button>
+      <input type="checkbox" />
     </div>
   );
 }
--- apps/color-button/src/App.test.js
+++ apps/color-button/src/App.test.js
@@ -4,24 +4,44 @@ import App from "./App";
 test("the button has the correct initial color and text", () => {
   render(<App />);
 
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to whitesmoke",
+  });
 
-  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "rebeccapurple" });
 });
-test("the button clicked turns to blues", () => {
+test("the button clicked turns to whitesmokes", () => {
   render(<App />);
 
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to whitesmoke",
+  });
 
   fireEvent.click(colorButton);
 
-  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "whitesmoke" });
 });
 test("the button clicked change his text content", () => {
   render(<App />);
 
-  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", {
+    name: "Change to whitesmoke",
+  });
 
   fireEvent.click(colorButton);
-  expect(colorButton.textContent).toBe("Change to red");
+  expect(colorButton.textContent).toBe("Change to rebeccapurple");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+
+  //check that the button starts enabled
+  const colorButton = screen.getByRole("button", {
+    name: "Change to whitesmoke",
+  });
+  expect(colorButton).toBeEnabled();
+
+  //check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
 });
