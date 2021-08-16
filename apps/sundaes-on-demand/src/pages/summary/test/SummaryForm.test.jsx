fc02f4de7bc31b237e9f09811fe311bfef225afb
Julien Orjollet <oj.professionnel@gmail.com>
2021-08-16 20:57:03 +0200

    implemented firsts tests on SummaryForm, testing the checkbox btn

---
+++ apps/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import SummaryForm from "../SummaryForm"
+
+test("Initial Condition", () => {
+  render(<SummaryForm />)
+
+  const checkboxBtn = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  })
+  expect(checkboxBtn).not.toBeChecked()
+
+  const confirmBtn = screen.getByRole("button", {
+    name: /confirm order/i,
+  })
+  expect(confirmBtn).toBeDisabled()
+})
+test("Checkbox disables button on first click then enables on second click", () => {
+  render(<Summaryform />)
+
+  const checkboxBtn = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  })
+  const confirmBtn = screen.getByRole("button", {
+    name: /confirm order/i,
+  })
+  fireEvent.click(checkboxBtn)
+  expect(confirmBtn).toBeEnabled()
+
+  fireEvent.click(checkboxBtn)
+  expect(confirmBtn).toBeDisabled()
+})
