b02288547e90862753b280dee40b2dd8a600c57b
Julien Orjollet <oj.professionnel@gmail.com>
2021-08-17 11:49:50 +0200

    implemented tests for popover react-bootstrap element on sundaes app

--- apps/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ apps/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -30,3 +30,27 @@ test("Checkbox disables button on first click then enables on second click", () 
   userEvent.click(checkboxBtn)
   expect(confirmBtn).toBeDisabled()
 })
+it("should not show the PopOver text on start", () => {
+  render(<SummaryForm />)
+  //popover starts out hidden
+  const popoverNullDefaultVal = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  )
+  expect(popoverNullDefaultVal).toBeNull()
+})
+it("should show the PopOver text on hover and disappear when out", () => {
+  render(<SummaryForm />)
+  //popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  userEvent.hover(termsAndConditions)
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i)
+  expect(popover).toBeInTheDocument()
+
+  //popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions)
+  const popoverBackToNull = screen.queryByText(
+    /no ice cream will be delivered/i
+  )
+  expect(popoverBackToNull).toBeNull()
+})
